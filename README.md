d35ff670cb9a3faf9725eb90770e4555f93df8b3
AlexBepple <axbepp@gmail.com>
2023-03-11 17:04:18 -0800

    Added connection to the mongo atlas DB. The pet data is now coming from there. I need to implement functionality to add to the DB when using add item.

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # PetStore
 Modern Pet Store Website
 
-
 ## Setting up the server 
 'cd server
 npm install 
+npm install mongo
 node server.js'
 
 ## Rebuilding the server 
@@ -17,3 +17,30 @@ npm run build
 '
 ## probably need to run this
 'npm install react-router-dom --save'
+
+# MONGOSH -- this is for database manipulation in the terminal outside of the app and is not necessary for app function.
+Can use this to conenct and view the data in the db
+From an elivated powershell - (choco is an admin tool that comes with node js but must be run with admin privledges)
+'choco install mongodb-atlas'
+https://www.mongodb.com/docs/mongodb-shell/install/#procedure
+go to this link and get mongodbshell
+connect to the db
+'mongosh "mongodb+srv://databaseone.ayfgx0w.mongodb.net/myFirstDatabase" --apiVersion 1 --username dbuser'
+password is: MangoDeezBees
+## Once in the mongosh shell
+'show dbs' shows the available databases
+'use Petstore' selects the petstore db as the current db
+'show collections' shows the collections of data in the petstore
+'exit' ends the mongoshell connection
+## Your current database is db
+'db' you can use this command to see your working db
+## to insert a document into the lists collection in the petstore we simply
+db.lists.insertOne({name: "Snowy", type: "dog"})
+## to view a collection 
+db.lists.find()
+## can query with the find and select based on type for example
+db.lists.find().sort({type: -1})
+
+# TODO
+when add item add the item to the database
+when add list add list to the database
--- server/server.js
+++ server/server.js
@@ -2,25 +2,45 @@ var express = require('express');
 var app = express();
 const path = require('path')
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://dbuser:MangoDeezBees@databaseone.ayfgx0w.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const dbName = "Petstore";
+const collectionName = "lists";
+
+const database = client.db(dbName);
+const collection = database.collection(collectionName);
+
 app.use(express.static(path.join(__dirname, '..', 'lists', 'build')));
 
-app.use('/adminData', (req, res) => {
-    console.log('admin');
-    const data = {
-      lists: ['Cats', 'Dogs', 'Birds'],
-      items: { 
-        Cats: [{name: "Felidae"}, {name: "Garfiled"}, {name: "Cat in theHat"}],
-        Dogs: [{name: "Snoopy"}, {name: "Lola"}, {name: "Sprinkles"}],
-        Birds: [{name: "Tweety"}, {name: "Squawk"}, {name: "Chirp"}]}
-    };
-    res.json(data);
-  });
+app.use('/adminData', async (req, res) => {
+  try {
+    await client.connect();
+    var cursor = await collection.find({}, { projection: { _id: 0, list: 1, items: 1 } });
+    var index = 0;
+    var ray = await cursor.toArray();
+    var data = {lists: Array(ray.length), items: Object()};
+    ray.forEach(obj => {
+      data.lists[index] = obj.list;
+      data.items[obj.list] = obj.items;
+      index += 1;
+    });
+  } catch (err) {
+    console.log("Sumn is not right");
+  } finally {
+    await client.close();
+  }
+  res.json(data);
+});
 
 
 app.use('*', function (req, res) {
-    res.sendFile(path.join(__dirname, '..', 'lists', 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'lists', 'build', 'index.html'));
 });
 
 app.listen(3000, () => {
-    console.log('Listening on port 3000');
+  console.log('Listening on port 3000');
 });
+
+
